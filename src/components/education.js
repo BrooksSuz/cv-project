d0af53d96d9c65cc106516830c91e7f6adef67fc
Brooks Susor <susorbrooks@gmail.com>
2022-11-09 12:37:54 -0500

    Initialize App styles

--- src/components/education.js
+++ src/components/education.js
@@ -16,11 +16,11 @@ class Education extends Component {
           <label htmlFor="school-name">School Name:</label>
           <input type="text" id="school-name" name="school-name"></input>
         </div>
-        <div>
+        <div className="area-of-study-container">
           <label htmlFor="area-of-study">Area of Study:</label>
           <input type="text" id="area-of-study" name="area-of-study"></input>
         </div>
-        <div>
+        <div className="date-of-study-container">
           <label htmlFor="date-of-study">Date of Study:</label>
           <input type="date" id="date-of-study" name="date-of-study"></input>
         </div>
